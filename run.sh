305366036dc5462b0a731a13bb5ed81ccde93799
wanghaisheng <edwin.uestc@gmail.com>
2022-04-07 07:48:47 +0800

    no domain issue

--- run.sh
+++ run.sh
@@ -32,9 +32,9 @@ title_file=${domain}'/'${domain}'_title.txt'
 # 各自命令定制
 # 由于目前 httpx 输出文件如果不加 -no-color 参数，保存结果文件会包含颜色代码,故使用perl替换了一下后再保存
 
-sub="./subfinder -d ${domain} -o ${sub_file} -silent"
-ksubdomain="sudo ./ksubdomain v -b 20M -f ${sub_file} --od -o ${sub_file_ok}"
-httpx="./httpx -t 30 -rl 60 -fr -maxr 3 -retries 3 -timeout 3 -ec -sc -cl -title -l ${sub_file_ok} -o tmp.txt &&cat tmp.txt|perl -pe 's/\e\[[0-9;]*m//g'|tee ${title_file}>>/dev/null 2>&1"
+sub="bash ./subfinder -d ${domain} -o ${sub_file} -silent"
+ksubdomain="bash ./ksubdomain v -b 20M -f ${sub_file} --od -o ${sub_file_ok}"
+httpx="bash ./httpx -t 30 -rl 60 -fr -maxr 3 -retries 3 -timeout 3 -ec -sc -cl -title -l ${sub_file_ok} -o tmp.txt &&cat tmp.txt|perl -pe 's/\e\[[0-9;]*m//g'|tee ${title_file}>>/dev/null 2>&1"
 # -proxy http://127.0.0.1:8080 
 
 # 如果子域名为空，删掉结果
@@ -45,7 +45,7 @@ if [ -s ${title_file} ];then
     printf "=>>= 共 `wc -l ${sub_file}|awk '{print $1}'` 条子域名 \n"
     printf "=>>= 共 `wc -l ${sub_file_ok}|awk '{print $1}'` 条有效子域名 \n"
     printf "=>>= 共 `wc -l ${title_file}|awk '{print $1}'` 条可正常访问域名 \n"
-    sudo chown -R `whoami` ${title_file}
+    chown -R `whoami` ${title_file}
 else
     printf "[+] 没有获取到域名 <<${domain}>> 的任何有效域名！ \n"
     rm -rf ${domain}
