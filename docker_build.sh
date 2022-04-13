28b6b6c17df8dc1c007b27c81d61b8c2fa3a7268
wanghaisheng <edwin.uestc@gmail.com>
2022-04-14 04:41:48 +0800

    mv to usr/local/bin

--- docker_build.sh
+++ docker_build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-chmod 777 ./ksubdomain && ls -al ./ksubdomain
-chmod 777 ./httpx
-chmod 777 ./subfinder
+
 cp ./ksubdomain /usr/local/bin
+chmod 777 /usr/local/bin/ksubdomain
 cp ./httpx /usr/local/bin
+chmod 777 /usr/local/bin/httpx
 cp ./subfinder /usr/local/bin
+chmod 777 /usr/local/bin/subfinder
